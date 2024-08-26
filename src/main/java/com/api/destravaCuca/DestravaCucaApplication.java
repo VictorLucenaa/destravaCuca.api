3a40e904e0d91e9f709616777a3607caddc0a58a
Victor Lucena <victor.lucenaaa@gmail.com>
2024-08-26 00:04:48 -0300

    initial configuration

---
+++ src/main/java/com/api/destravaCuca/DestravaCucaApplication.java
@@ -0,0 +1,13 @@
+package com.api.destravaCuca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DestravaCucaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DestravaCucaApplication.class, args);
+	}
+
+}
