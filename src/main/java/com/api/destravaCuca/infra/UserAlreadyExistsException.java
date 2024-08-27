83a5373fa0ea48ebc6eecb110b9ace0d0a99e2dc
Victor Lucena <victor.lucenaaa@gmail.com>
2024-08-27 12:55:22 -0300

    feat: create UserAlreadyExistsException and UserNotFoundException

---
+++ src/main/java/com/api/destravaCuca/infra/UserAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.api.destravaCuca.infra;
+
+public class UserAlreadyExistsException extends RuntimeException{
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+}
